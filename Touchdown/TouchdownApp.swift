c12bf11b07dd11e37ce50e0d43b2b2ee8dfd3420
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2023-02-04 05:37:58 -0300

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Orlando Moraes Martins on 04/02/23.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
